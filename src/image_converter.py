2ee31b42d677716801f1516ff9a697abdc39949d
Evan Gillespie <evan.gillespie1@gmail.com>
2014-08-21 08:19:20 -0600

    fixed a width/height mixup in the resizer

--- src/image_converter.py
+++ src/image_converter.py
@@ -29,7 +29,8 @@ class ImageConverter(object):
 		"""
 		resize an image object to the size defined in the config and return it.
 		"""
-		old_h, old_w = img.size
+		# TODO: make sure that the new width is never less than it should be (rounding error)
+		old_w, old_h = img.size
 		resize_ratio = float(WIDTH) / float(old_w)
 		img = img.resize((int(old_w*resize_ratio), int(old_h*resize_ratio)), Image.ANTIALIAS)
 
@@ -45,8 +46,3 @@ class ImageConverter(object):
 		new_h = HEIGHT
 		new_w = WIDTH
 
-		if old_h > new_h:
-			#crop the height
-			pass
-
-		return img
