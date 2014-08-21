303c54a628a526a41b360115745d94eb8d53f46c
Evan Gillespie <evan@chaordix.com>
2014-08-21 11:01:13 -0600

    finished image resizing and cropping

---
+++ src/exceptions.py
@@ -0,0 +1,3 @@
+
+class ImageSizeError(Exception):
+    pass
--- src/image_converter.py
+++ src/image_converter.py
@@ -1,6 +1,7 @@
 
 from PIL import Image
 from .config import WIDTH, HEIGHT
+from .exceptions import ImageSizeError
 
 __author__ = ('evan',)
 
@@ -21,6 +22,7 @@ class ImageConverter(object):
 		"""
 		img = cls._resize_image(img)
 		img = cls._crop_image(img)
+		cls._verify_final_dimensions(img)
 
 		return img
 
@@ -29,11 +31,10 @@ class ImageConverter(object):
 		"""
 		resize an image object to the size defined in the config and return it.
 		"""
-		# TODO: make sure that the new width is never less than it should be (rounding error)
 		old_w, old_h = img.size
-		resize_ratio = float(WIDTH) / float(old_w)
+		resize_ratio = max([float(WIDTH)/float(old_w), float(HEIGHT)/float(old_h)])
 		img = img.resize((int(old_w*resize_ratio), int(old_h*resize_ratio)), Image.ANTIALIAS)
-
+		print img
 		return img
 
 	@classmethod
@@ -42,7 +43,35 @@ class ImageConverter(object):
 		crop an image to the ratio defined in the config and return it.
 		Image is not resized here. Assume that the image is the right size(ish) already
 		"""
-		old_h, old_w = img.size
+		old_w, old_h = img.size
 		new_h = HEIGHT
 		new_w = WIDTH
+		top = 0
+		bottom = old_h
+		left = 0
+		right = old_w
 
+		if old_h > new_h:
+			diff = old_h-new_h
+			top = diff/2
+			bottom = old_h-diff/2
+			if bottom-top > new_h:
+				bottom -= bottom-top-new_h
+		if old_w > new_w:
+			diff = old_w-new_w
+			left = diff/2
+			right = old_w-diff/2
+			if right-left > new_w:
+				right -= right-left-new_w
+
+		return img.crop((left, top, right, bottom))
+
+	@classmethod
+	def _verify_final_dimensions(cls, img):
+		"""
+		check that the final image is the correct size.
+		raise an exception if it isnt
+		"""
+		w, h = img.size
+		if w != WIDTH or h != HEIGHT:
+			raise ImageSizeError("your image is the wrong size. It is: %s x %s and should be: %s x %s" % (w, h, WIDTH, HEIGHT))
